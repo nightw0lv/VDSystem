377fea9c5ec72101e4a10e483243c8d7413de9cd
nightw0lv <dotaddict69@hotmail.gr>
2023-05-27 07:28:40 +0300

    Grand Crusade Source update to 1.6
    - Version 1.6
    - Refactor Config for item delivery manager
    - Update SQL enum in vds_* tables for new topsites
    - Update millisecondsFromString() will print ServerTime and TimeZone on Gui
    - Update getMyIP() ip check with SSL
    - Added HOPZONE.EU topsite
    - Added Individual URL
    - Added global URL
    - Added URL generation to fix IPv6 voter problem
    - Added HOTSERVERS.ORG topsite
    - Added Individual URL
    - Added global URL
    - Removed L2TOPSERVERS.COM
    - Refactor for Hopzone into Hopzone Net
    - Updated discord invitation
    - Updated GitHub URL
    - Added DenArt Designs panel URLs
    - Fix checking array length to avoid possible errors when checking for json
    - Added Gui Messages for website connection failures
    - Added .vote command
    - Window open showing the enabled vote commands
    - Shows player IP Address
    - Ignores Flood protection
    - Split individual response on connection Execution method to implement the new way of IPv6 method fix
    - GenerateVoteURL method will create a new url for player to vote
    - Currently only new hopzone supports this
    - New custom message on vote failure for the generated URL
    
    Note: This update finds L2JBrasil with broken Global API. (yes they broke it)
    Note: This update implements a new way for IPv6 players to get their reward
    Note: The new implementation works only on Hopzone right now.
    Note: The new Implementation is not up to this system in order to work but each topsite.
    
    TODO: Old lineage 2 clients don't support URL open in browser for players by default so its up to you to open the generated URL's on those chronicles with exception to Lucera that does that by default. There are code hints on where you should do that notably VoteCMD.java, these functions are also provided by some client protections like smartguard, sguard, and strixguard.

--- Mobius/L2J_Mobius_04.0_GrandCrusade/src/hopzone/eu/util/Logs.java
+++ Mobius/L2J_Mobius_04.0_GrandCrusade/src/hopzone/eu/util/Logs.java
@@ -33,7 +33,7 @@ import java.util.logging.Logger;
  * VDS Stands for: Vote Donation System
  * Script website: https://itopz.com/
  * Partner website: https://hopzone.eu/
- * Script version: 1.5
+ * Script version: 1.6
  * Pack Support: Mobius 04.0 GrandCrusade
  * <p>
  * Freemium Donate Panel V4: https://www.denart-designs.com/
